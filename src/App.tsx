46ef8d2a983aa0fa5580ec08ec2b5a567c73169b
Joseph Wang <jgwang@ad.unc.edu>
2023-12-25 02:30:41 -0500

    Temporarily remove some linting rules for test deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Christmas2023, ConnectionsGame } from './data/GameBank'
 import WordBlock from './components/WordBlock'
